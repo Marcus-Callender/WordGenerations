6dafe0b657906427bc29bbf299cd7c1d77e9772b
Marcus Callender <m.shute@outlook.com>
2017-04-19 12:32:42 +0100

    Implemented add, remove and change muttionns and tested them

--- WordGenerations/WordGenerations/Word.py
+++ WordGenerations/WordGenerations/Word.py
@@ -1,37 +1,43 @@
 import random
 
 class CWord:
-    mutateChangePercent = 12.0;
-    mutateAddPercent = 3.0;
-    mutateRemovePercent = 3.0;
-    mutateSwapPercent = 3.0;
 
     def __init__(self):
         self.contents = "";
         self.length = 0;
 
-        wordMinLength = 2;
-        wordMaxLength = 12;
+        self.wordMinLength = 2;
+        self.wordMaxLength = 12;
 
-        self.length = random.randrange(wordMinLength, wordMaxLength);
+        self.mutateChangePercent = 12.0;
+        self.mutateAddPercent = 3.0;
+        self.mutateRemovePercent = 3.0;
+        self.mutateSwapPercent = 3.0;
+
+        self.length = random.randrange(self.wordMinLength, self.wordMaxLength);
 
         for z in range(self.length):
             self.contents += chr(random.randrange(97, 123));
 
     def mutate(self):
+        temp = self.contents;
         roll = random.randrange(100);
 
-        if (roll < mutateChangePercent):
-            mutateChange();
+        if (roll < self.mutateChangePercent):
+            self.mutateChange();
+            print("Change: " + temp + " -> " + self.contents);
 
-        elif (roll < (mutateChangePercent + mutateAddPercent)):
-            mutateAdd();
+        elif (roll < (self.mutateChangePercent + self.mutateAddPercent)):
+            self.mutateAdd();
+            print("Add: " + temp + " -> " + self.contents);
 
-        elif (roll < (mutateChangePercent + mutateAddPercent + mutateRemovePercent)):
-            mutateRemove();
+        elif (roll < (self.mutateChangePercent + self.mutateAddPercent + self.mutateRemovePercent)):
+            self.mutateRemove();
+            print("Remove: " + temp + " -> " + self.contents);
 
-        elif (roll < (mutateChangePercent + mutateAddPercent + mutateRemovePercent + mutateSwapPercent)):
-            mutateSwap();
+        elif (roll < (self.mutateChangePercent + self.mutateAddPercent + self.mutateRemovePercent + self.mutateSwapPercent)):
+            self.mutateSwap();
+            #print("Swap: " + temp + " -> " + self.contents);
 
     def conection(self, wordToFind):
         pass;
@@ -43,16 +49,19 @@ class CWord:
         while(self.contents[mutatedIndex] == mutatedChar):
             mutatedChar = chr(random.randrange(97, 123));
 
-        self.contents[mutatedIndex] = mutatedChar;
+        #self.contents[mutatedIndex] = mutatedChar;
+        self.contents = self.contents[:mutatedIndex - 1] + mutatedChar + self.contents[mutatedIndex:];
 
     def mutateAdd(self):
-        if (self.length < wordMaxLength):
+        if (self.length < self.wordMaxLength):
             randomLetter = chr(random.randrange(97, 123));
             posInString = random.randrange(0, self.length + 1);
             self.contents = self.contents[:posInString] + randomLetter + self.contents[posInString:];
 
     def mutateRemove(self):
-        pass;
+        if (self.length > self.wordMinLength):
+            randomPos = random.randrange(1, len(self.contents));
+            self.contents = self.contents[:randomPos - 1] + self.contents[randomPos:];
 
     def mutateSwap(self):
         pass;
--- WordGenerations/WordGenerations/WordGenerations.py
+++ WordGenerations/WordGenerations/WordGenerations.py
@@ -20,17 +20,37 @@ def main():
         else:
             print(', ', end = '');
 
-    hello = "hello";
+    for z in range(NumberOfWords):
+        words[z].mutate();
 
-    hello = Add(0, 'Y', hello);
-    print(hello);
+    for z in range(NumberOfWords):
+        words[z].display();
 
-    hello = Add(7, 'Y', hello);
-    print(hello);
+        if (z == NumberOfWords - 1):
+            print('\n', end = '');
+        else:
+            print(', ', end = '');
 
-    hello = Add(5, 'Y', hello);
-    print(hello);
-            
+    #hello = "hello";
+    #
+    #hello = Add(0, 'Y', hello);
+    #print(hello);
+    #
+    #hello = Add(7, 'Y', hello);
+    #print(hello);
+    #
+    #hello = Add(5, 'Y', hello);
+    #print(hello);
+    #
+    #hello = remove(2, hello);
+    #print(hello);     
+    #
+    #hello = remove(1, hello);
+    #print(hello);
+    #
+    #hello = remove(6, hello);
+    #print(hello);   
+
 def Add(pos, letter, string):
     randomLetter = chr(random.randrange(97, 123));
     posInString = random.randrange(0, len(string));
@@ -48,7 +68,14 @@ def Add(pos, letter, string):
     #    pass;
 
     return string;
-    pass;
+
+def remove(pos, string):
+    string = string[:pos - 1] + string[pos:];
+
+    return string;
+
+def swap(pos1, pos2, string):
+    return string;
     
 
 
